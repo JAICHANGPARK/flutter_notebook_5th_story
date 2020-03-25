7bf99e1c3eb782b5faeaf0019ee8acf69f29400d
Dreamwalker <aristojeff@gmail.com>
2020-03-25 20:13:44 +0900

    change lib folder

---
+++ lib/ep401_dog_sitters_app/main_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class DogSittersApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: DogSitterMainPage(),
+    );
+  }
+}
+
