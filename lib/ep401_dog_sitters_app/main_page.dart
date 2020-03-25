e2bd8cde6c1f020fbd9f37f23c46ce5ad2464ed2
Dreamwalker <aristojeff@gmail.com>
2020-03-25 20:17:04 +0900

    Add s

--- lib/ep401_dog_sitters_app/main_page.dart
+++ lib/ep401_dog_sitters_app/main_page.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 
 class DogSittersApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -12,3 +12,34 @@ class DogSittersApp extends StatelessWidget {
   }
 }
 
+class DogSitterMainPage extends StatefulWidget {
+  @override
+  _DogSitterMainPageState createState() => _DogSitterMainPageState();
+}
+
+class _DogSitterMainPageState extends State<DogSitterMainPage> {
+//20 59 62
+  Color appColor = Color(0xff143b3e);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: appColor,
+                  borderRadius: BorderRadius.only(bottomRight: Radius.circular(24), bottomLeft: Radius.circular(24))),
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Placeholder(),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutternotebook5th/ep401_dog_sitters_app/main_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(DogSittersApp());
 }
