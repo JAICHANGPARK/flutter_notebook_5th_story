9e218b5459cc2625beb151cb452544bf09ea5d9f
Dreamwalker <aristojeff@gmail.com>
2020-03-28 21:00:11 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutternotebook5th/ep401_dog_sitters_app/main_page.dart';
+import 'package:flutternotebook5th/ep404_audiobook_app/main_page.dart';
 
-void main() {
-  runApp(DogSittersApp());
-}
+void main() => runApp(AudioBookApp());
