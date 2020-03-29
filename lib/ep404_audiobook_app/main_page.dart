9331665c6a4fdbe68ddd7cd5571a49604ef709f1
Dreamwalker <aristojeff@gmail.com>
2020-03-29 18:09:24 +0900

    Update main_page.dart

--- lib/ep404_audiobook_app/main_page.dart
+++ lib/ep404_audiobook_app/main_page.dart
@@ -17,6 +17,7 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   double _sliderValue = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,71 +25,70 @@ class _MainPageState extends State<MainPage> {
         child: Column(
           children: <Widget>[
             Expanded(
-              flex: 2,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      width: 32,
-                      decoration: BoxDecoration(
-                        color: Colors.orange,
-                        shape: BoxShape.circle,
-                      ),
-                      child: Center(
-                        child: Icon(
-                          Icons.keyboard_arrow_left,
-                          color: Colors.white,
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        width: 32,
+                        decoration: BoxDecoration(
+                          color: Colors.orange,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Center(
+                          child: Icon(
+                            Icons.keyboard_arrow_left,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
-                    ),
-                    Spacer(),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Icon(Icons.directions_car,color: Colors.grey,),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Icon(Icons.access_time,color: Colors.grey),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Icon(Icons.more_horiz,color: Colors.grey),
-                    )
-                  ],
-                ),
-              )
-            ),
+                      Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(
+                          Icons.directions_car,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(Icons.access_time, color: Colors.grey),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(Icons.more_horiz, color: Colors.grey),
+                      )
+                    ],
+                  ),
+                )),
             Expanded(
               flex: 20,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                    Text("Shared library"),
-                    Text("19 listening")
-                  ],),
+                    children: <Widget>[Text("Shared library"), Text("19 listening")],
+                  ),
                   Container(
                     margin: EdgeInsets.symmetric(horizontal: 64, vertical: 16),
                     height: MediaQuery.of(context).size.height / 3,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(24),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey[400],
-                          blurRadius: 5,
-                          spreadRadius: 2,
-                          offset: Offset(1,2),
-                        )
-                      ],
-                      image: DecorationImage(
-                        image: NetworkImage("https://cdn.pixabay.com/photo/2020/03/15/15/41/medical-4934010_960_720.jpg"),
-                        fit: BoxFit.cover,
-                      )
-                    ),
+                        borderRadius: BorderRadius.circular(24),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey[400],
+                            blurRadius: 5,
+                            spreadRadius: 2,
+                            offset: Offset(1, 2),
+                          )
+                        ],
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              "https://cdn.pixabay.com/photo/2020/03/15/15/41/medical-4934010_960_720.jpg"),
+                          fit: BoxFit.cover,
+                        )),
                   ),
                   Text("The Art Of Making Memories"),
                   Text("Meik Wiking"),
@@ -96,17 +96,22 @@ class _MainPageState extends State<MainPage> {
                     activeColor: Colors.orange,
                     inactiveColor: Colors.orange[100],
                     onChanged: (double value) {
-                    setState(() {
-                      _sliderValue = value;
-                    });
-                  }, value: _sliderValue,
+                      setState(() {
+                        _sliderValue = value;
+                      });
+                    },
+                    value: _sliderValue,
                   ),
-Row(
-  children: <Widget>[
-    
-  ],
-)
-
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text("00:40:04"),
+                        Text("03:40:00"),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
@@ -115,12 +120,11 @@ Row(
               child: Container(
                 padding: EdgeInsets.symmetric(horizontal: 32),
                 decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(24),
-                    topRight: Radius.circular(24),
-                  )
-                ),
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(24),
+                      topRight: Radius.circular(24),
+                    )),
                 child: Row(
                   mainAxisSize: MainAxisSize.max,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -130,40 +134,56 @@ Row(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Icon(Icons.home, color: Colors.grey,),
-                        Text("Feed",style: TextStyle(
-                          fontSize: 12
-                        ),),
+                        Icon(
+                          Icons.home,
+                          color: Colors.grey,
+                        ),
+                        Text(
+                          "Feed",
+                          style: TextStyle(fontSize: 12),
+                        ),
                       ],
                     ),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Icon(Icons.tune,color: Colors.grey,),
-                        Text("My Shelf",style: TextStyle(
-                            fontSize: 12
-                        ),),
+                        Icon(
+                          Icons.tune,
+                          color: Colors.grey,
+                        ),
+                        Text(
+                          "My Shelf",
+                          style: TextStyle(fontSize: 12),
+                        ),
                       ],
                     ),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Icon(Icons.play_arrow,color: Colors.orange,),
-                        Text("Play",style: TextStyle(
-                            fontSize: 12
-                        ),),
+                        Icon(
+                          Icons.play_arrow,
+                          color: Colors.orange,
+                        ),
+                        Text(
+                          "Play",
+                          style: TextStyle(fontSize: 12),
+                        ),
                       ],
-                    )
-                    , Column(
+                    ),
+                    Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Icon(Icons.bookmark,color: Colors.grey,),
-                        Text("Notes",style: TextStyle(
-                            fontSize: 12
-                        ),),
+                        Icon(
+                          Icons.bookmark,
+                          color: Colors.grey,
+                        ),
+                        Text(
+                          "Notes",
+                          style: TextStyle(fontSize: 12),
+                        ),
                       ],
                     )
                   ],
@@ -176,20 +196,3 @@ Row(
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
