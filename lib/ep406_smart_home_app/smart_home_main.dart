dcfc6378fc42af464bedc1bdb5e2ce239535b3d3
Dreamwalker <aristojeff@gmail.com>
2020-03-30 20:02:11 +0900

    Update smart_home_main.dart

--- lib/ep406_smart_home_app/smart_home_main.dart
+++ lib/ep406_smart_home_app/smart_home_main.dart
@@ -3,6 +3,20 @@ import 'package:flutter/material.dart';
 class SmartHomeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SmartHomeSpeaker(),
+    );
+  }
+}
+class SmartHomeSpeaker extends StatefulWidget {
+  @override
+  _SmartHomeSpeakerState createState() => _SmartHomeSpeakerState();
+}
+
+class _SmartHomeSpeakerState extends State<SmartHomeSpeaker> {
+  @override
+  Widget build(BuildContext context) {
     return Container();
   }
 }
+
