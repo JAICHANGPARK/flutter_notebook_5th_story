0e0495a8513226d5f317a23c7a4e350ada87d323
Dreamwalker <aristojeff@gmail.com>
2020-03-30 20:05:41 +0900

    Update smart_home_main.dart

--- lib/ep406_smart_home_app/smart_home_main.dart
+++ lib/ep406_smart_home_app/smart_home_main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SmartHomeApp extends StatelessWidget {
@@ -37,9 +38,13 @@ class _SmartHomeSpeakerState extends State<SmartHomeSpeaker> {
             bottom: 0,
             child: Container(
               height: MediaQuery.of(context).size.height / 2 + 34,
-
               decoration: BoxDecoration(
-                color: Colors.white,
+                color: Colors.blueGrey[50],
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(24),
+                  topLeft: Radius.circular(24),
+                )
+
               ),
             ),
           )
