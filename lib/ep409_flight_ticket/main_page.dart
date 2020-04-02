8618d64da12d2fe41d139cb3a88bd3f8f04675c9
Dreamwalker <aristojeff@gmail.com>
2020-04-02 20:40:29 +0900

    Update main_page.dart

--- lib/ep409_flight_ticket/main_page.dart
+++ lib/ep409_flight_ticket/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -49,6 +50,26 @@ class _TicketPageState extends State<TicketPage> {
               child: Container(
                 decoration: BoxDecoration(color: Colors.blueAccent,
                 borderRadius: BorderRadius.circular(24),),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Container(
+                height: 64,
+                decoration: BoxDecoration(
+                  color: Colors.grey[50],
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(24),
+                    topRight: Radius.circular(24),
+                  )
+                ),
+                child: Row(
+                  children: <Widget>[
+
+                  ],
+                ),
               ),
             )
           ],
