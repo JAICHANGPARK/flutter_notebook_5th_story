aae2662a76d1c998c3b487e37f5249fa12f7fe3b
Dreamwalker <aristojeff@gmail.com>
2020-04-05 18:45:14 +0900

    Update main_page.dart

--- lib/ep412_book_class_app/main_page.dart
+++ lib/ep412_book_class_app/main_page.dart
@@ -4,6 +4,25 @@ import 'package:flutter/material.dart';
 class LoadBookingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        "/" : (context)=>BookingMainPage(),
+      },
+    );
   }
 }
+
+
+class BookingMainPage extends StatefulWidget {
+  @override
+  _BookingMainPageState createState() => _BookingMainPageState();
+}
+
+class _BookingMainPageState extends State<BookingMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
