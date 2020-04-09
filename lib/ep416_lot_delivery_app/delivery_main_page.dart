ad58762c18d51ca7943080721bd20fd13110bcde
Dreamwalker <aristojeff@gmail.com>
2020-04-09 20:10:23 +0900

    Adds

---
+++ lib/ep416_lot_delivery_app/delivery_main_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class DeliveryMainPage extends StatefulWidget {
+  @override
+  _DeliveryMainPageState createState() => _DeliveryMainPageState();
+}
+
+class _DeliveryMainPageState extends State<DeliveryMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/ep416_lot_delivery_app/main_page.dart
+++ lib/ep416_lot_delivery_app/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutternotebook5th/ep416_lot_delivery_app/delivery_main_page.dart';
 
 
 class IoTDeliveryTrackingApp extends StatelessWidget {
