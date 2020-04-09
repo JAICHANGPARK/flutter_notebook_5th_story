00a4fc68e02acc4df5baac67f1c1ffd8cbb297f1
Dreamwalker <aristojeff@gmail.com>
2020-04-09 20:09:37 +0900

    Adds

---
+++ lib/ep416_lot_delivery_app/main_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class IoTDeliveryTrackingApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DeliveryMainPage(),
+    );
+  }
+}
