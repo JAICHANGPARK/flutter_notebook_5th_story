88a756cea8fa04111f9390f344587d2370f0cb5e
Dreamwalker <aristojeff@gmail.com>
2020-04-12 20:23:41 +0900

    Update boarding_ticket_page.dart

--- lib/ep419_boarding_pass_app/boarding_ticket_page.dart
+++ lib/ep419_boarding_pass_app/boarding_ticket_page.dart
@@ -4,6 +4,23 @@ import 'package:flutter/material.dart';
 class BoardingTicketPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Placeholder(),
+          ),
+          Expanded(
+            flex: 4,
+            child: Placeholder(),
+          ),
+          Expanded(
+            flex: 1,
+            child: Placeholder(),
+          )
+        ],
+      ),
+    );
   }
 }
