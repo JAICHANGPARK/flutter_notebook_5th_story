a9b6e722e8b92da548dd7e9cc5232e361039ac6f
Dreamwalker <aristojeff@gmail.com>
2020-04-12 20:21:08 +0900

    Create main_page.dart

---
+++ lib/ep419_boarding_pass_app/main_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class BoardingPassApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BoardingTicketPage(),
+    );
+  }
+}
+
+
