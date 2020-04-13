e3095702be8bf2ee970c575ab545a2ec028d597f
Dreamwalker <aristojeff@gmail.com>
2020-04-13 20:04:21 +0900

    Update ticket_clipper.dart

--- lib/ep419_boarding_pass_app/ticket_clipper.dart
+++ lib/ep419_boarding_pass_app/ticket_clipper.dart
@@ -8,6 +8,15 @@ class TicketClipper extends CustomClipper<Path>{
     path.lineTo(0.0, size.height);
     path.lineTo(size.width, size.height);
     path.lineTo(size.width, 0);
+
+    path.addOval(Rect.fromCircle(
+      center: Offset(0.0, size.height / 2),
+      radius: 24.0,
+    ));
+    path.addOval(Rect.fromCircle(
+      center: Offset(0.0, size.height / 2),
+      radius: 24.0,
+    ));
     return path;
   }
 
