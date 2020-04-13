99207b8fc20daf2405e3f197b1034f33f9e50c30
Dreamwalker <aristojeff@gmail.com>
2020-04-13 20:00:31 +0900

    Update ticket_clipper.dart

--- lib/ep419_boarding_pass_app/ticket_clipper.dart
+++ lib/ep419_boarding_pass_app/ticket_clipper.dart
@@ -4,13 +4,16 @@ class TicketClipper extends CustomClipper<Path>{
   @override
   Path getClip(Size size) {
     // TODO: implement getClip
-    throw UnimplementedError();
+    Path path = Path();
+    path.lineTo(0.0, size.height);
+    path.lineTo(size.width, size.height);
+    return path;
   }
 
   @override
   bool shouldReclip(CustomClipper<Path> oldClipper) {
     // TODO: implement shouldReclip
-    throw UnimplementedError();
+    return false;
   }
 
 }
