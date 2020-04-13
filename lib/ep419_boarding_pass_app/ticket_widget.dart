bcf1445c06c17b2bd6e1d1bbceecd6f8d74bfc77
Dreamwalker <aristojeff@gmail.com>
2020-04-13 19:57:12 +0900

    Create ticket_widget.dart

---
+++ lib/ep419_boarding_pass_app/ticket_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TicketWidget extends StatelessWidget {
+  final double width;
+  final double height;
+  final Widget child;
+  final bool cornerRounded;
+
+  TicketWidget(this.width, this.height, this.child, this.cornerRounded);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
