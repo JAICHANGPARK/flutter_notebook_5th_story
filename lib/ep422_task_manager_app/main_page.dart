5f4aa0ac7bd57b92e6eb57d1e398591503a7683a
Dreamwalker <aristojeff@gmail.com>
2020-04-15 19:21:49 +0900

    Update main_page.dart

--- lib/ep422_task_manager_app/main_page.dart
+++ lib/ep422_task_manager_app/main_page.dart
@@ -1,1 +1,23 @@
 import 'package:flutter/material.dart';
+
+class TaskManagerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TaskManagerMainPage(),
+    );
+  }
+}
+
+class TaskManagerMainPage extends StatefulWidget {
+  @override
+  _TaskManagerMainPageState createState() => _TaskManagerMainPageState();
+}
+
+class _TaskManagerMainPageState extends State<TaskManagerMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
