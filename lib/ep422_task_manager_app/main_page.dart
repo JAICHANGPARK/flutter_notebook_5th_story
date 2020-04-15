c897df5f4864ca28366cb45fe151db40ffc03f24
Dreamwalker <aristojeff@gmail.com>
2020-04-15 19:31:38 +0900

    Update main_page.dart

--- lib/ep422_task_manager_app/main_page.dart
+++ lib/ep422_task_manager_app/main_page.dart
@@ -50,43 +50,77 @@ class _TaskManagerMainPageState extends State<TaskManagerMainPage> {
                           Container(
                             height: 48,
                             width: 48,
-                            decoration: BoxDecoration(color: Colors.pink, borderRadius: BorderRadius.circular(8),
-                            image: DecorationImage(
-                              image: NetworkImage('https://avatars0.githubusercontent.com/u/19484515?s=460&u=0ec7b31ff9129826cccc5cd971887a9dd0e0a538&v=4')
-                            )),
-
+                            decoration: BoxDecoration(
+                                color: Colors.pink,
+                                borderRadius: BorderRadius.circular(8),
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                        'https://avatars0.githubusercontent.com/u/19484515?s=460&u=0ec7b31ff9129826cccc5cd971887a9dd0e0a538&v=4'))),
                           ),
                           Container(
                             height: 48,
                             width: 48,
                             decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(8)),
                             child: Center(
-                              child: Icon(Icons.calendar_today, color: Colors.white,),
+                              child: Icon(
+                                Icons.calendar_today,
+                                color: Colors.white,
+                              ),
                             ),
                           )
                         ],
                       ),
                     ),
-                    SizedBox(height: 24,),
-                    Text("Hello, Sandy", style: TextStyle(
-                      fontSize: 38,
-                      fontWeight: FontWeight.bold
-                    ),),
-                    SizedBox(height: 16,),
-                    Text("You have 5 task today!",style: TextStyle(
-                      fontSize: 18,
-
-                    ),),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Text(
+                      "Hello, Sandy",
+                      style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Text(
+                      "You have 5 task today!",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
                   ],
                 ),
               ),
             ),
+            Positioned(
+              left: 0,
+              right: 0,
+              top: MediaQuery.of(context).size.height / 3.7,
+              child: Container(
+                height: 100,
+                decoration: BoxDecoration(
+                  border: Border.all()
+                ),
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index){
+                  return Container(
+                    width: 64,
+                    color: Colors.black,
+                    margin: EdgeInsets.only(right: 16),
+                  );
+                }),
+              ),
+
+            )
           ],
         ),
       ),
     );
   }
 }
+
+
+
 
 
 
