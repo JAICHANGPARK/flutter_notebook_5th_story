6dc79c0f3253d0fa015810eb37077d387c6aa7ae
Dreamwalker <aristojeff@gmail.com>
2020-04-18 18:37:54 +0900

    Update main_page.dart

--- lib/ep425_daily_activity_app/main_page.dart
+++ lib/ep425_daily_activity_app/main_page.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 class DailyActivityApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: DailyMainPage(),
+    );
   }
 }
+
