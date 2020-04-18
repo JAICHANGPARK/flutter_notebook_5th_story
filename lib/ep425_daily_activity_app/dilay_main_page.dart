899942350f972f0b9a595ef7bf84cb077cc1414f
Dreamwalker <aristojeff@gmail.com>
2020-04-18 18:44:59 +0900

    Update dilay_main_page.dart

--- lib/ep425_daily_activity_app/dilay_main_page.dart
+++ lib/ep425_daily_activity_app/dilay_main_page.dart
@@ -3,35 +3,41 @@ import 'package:flutter/material.dart';
 class DailyMainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height / 3,
-              child: Placeholder(),
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height / 3,
-              child: Placeholder(),
-            ),
-            Row(
-              children: [
-                Text("Today's activitys"),
-                Container(
-                  height: 24,
-                  width: 48,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(color: Colors.deepOrange)
+    // 5e 49 c5
+    return Theme(
+      data: ThemeData(
+        backgroundColor: Color(0xFF5E49C5),
+      ),
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                child: Placeholder(),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                child: Placeholder(),
+              ),
+              Row(
+                children: [
+                  Text("Today's activitys"),
+                  Container(
+                    height: 24,
+                    width: 48,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.deepOrange)
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height / 2,
-            )
-          ],
+                ],
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 2,
+              )
+            ],
+          ),
         ),
       ),
     );
