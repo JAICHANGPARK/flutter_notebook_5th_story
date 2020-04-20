3f802094022adb0ebb2ff4cabd785b4b4322fc65
Dreamwalker <aristojeff@gmail.com>
2020-04-20 21:12:01 +0900

    Adds

--- lib/ep425_daily_activity_app/main_page.dart
+++ lib/ep425_daily_activity_app/main_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutternotebook5th/ep425_daily_activity_app/dilay_main_page.dart';
 
 
 class DailyActivityApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
       home: DailyMainPage(),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutternotebook5th/ep425_daily_activity_app/main_page.dart';
 
 void main(){
-  SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
+
   runApp(DailyActivityApp());
 
 
