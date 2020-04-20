9f643a9d14bb8a92c15a20dd4d1d3c64d1dd2a5e
Dreamwalker <aristojeff@gmail.com>
2020-04-20 20:38:45 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutternotebook5th/ep425_daily_activity_app/main_page.dart';
 
-void main() => runApp(DailyActivityApp());
+void main(){
+  SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
+  runApp(DailyActivityApp());
+
+
+}
