048f6f4037c1966259c99de9d89906d184bceae2
Dreamwalker <aristojeff@gmail.com>
2020-04-21 19:44:39 +0900

    Update main_page.dart

--- lib/ep425_daily_activity_app/main_page.dart
+++ lib/ep425_daily_activity_app/main_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutternotebook5th/ep425_daily_activity_app/dilay_main_page.dart';
 
-
 class DailyActivityApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,4 +11,3 @@ class DailyActivityApp extends StatelessWidget {
     );
   }
 }
-
