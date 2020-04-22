dfadd1a724037225cf19e768be96cfdf973d1432
Dreamwalker <aristojeff@gmail.com>
2020-04-22 19:41:52 +0900

    Update main_page.dart

--- lib/ep429_fasion_store_app/main_page.dart
+++ lib/ep429_fasion_store_app/main_page.dart
@@ -33,14 +33,19 @@ class _FasionMainPageState extends State<FashionMainPage> {
               topRight: Radius.circular(24)
             )
           ),
+          padding: EdgeInsets.symmetric(horizontal: 24),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("FG"),
+              Text("FG", style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20
+              ),),
               IconButton(icon: Icon(Icons.favorite_border),
               onPressed: (){},),
               IconButton(icon: Icon(Icons.card_travel),
                 onPressed: (){},),
-              IconButton(icon: Icon(Icons.favorite_border),
+              IconButton(icon: Icon(Icons.person_outline),
                 onPressed: (){},),
 
             ],
