fc8e2e9f954815f593e00b6881c23aef727a0c0a
Dreamwalker <aristojeff@gmail.com>
2020-04-22 19:34:39 +0900

    Update main_page.dart

--- lib/ep429_fasion_store_app/main_page.dart
+++ lib/ep429_fasion_store_app/main_page.dart
@@ -3,6 +3,21 @@ import 'package:flutter/material.dart';
 class FashionStoreApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: FashionMainPage(),
+    );
   }
 }
+
+class FashionMainPage extends StatefulWidget {
+  @override
+  _FasionMainPageState createState() => _FasionMainPageState();
+}
+
+class _FasionMainPageState extends State<FashionMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
