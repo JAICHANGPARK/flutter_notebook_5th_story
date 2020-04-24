72261c00a8eb8705036d74a50ac380e5943bdde6
Dreamwalker <aristojeff@gmail.com>
2020-04-24 20:43:12 +0900

    Update second_page.dart

--- lib/ep429_fasion_store_app/second_page.dart
+++ lib/ep429_fasion_store_app/second_page.dart
@@ -3,12 +3,10 @@ import 'package:flutter/material.dart';
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-          color: Colors.green,
-        ),
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        color: Colors.green,
       ),
     );
   }
