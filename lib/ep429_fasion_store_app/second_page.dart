baf91ebfa3b4988ded899e02da156328e53db4b2
Dreamwalker <aristojeff@gmail.com>
2020-04-24 20:47:08 +0900

    Update second_page.dart

--- lib/ep429_fasion_store_app/second_page.dart
+++ lib/ep429_fasion_store_app/second_page.dart
@@ -4,34 +4,36 @@ import 'package:flutter/material.dart';
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      decoration: BoxDecoration(
-        color: Colors.green,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Looks", style: TextStyle(
-            fontSize: 18,
+    return SafeArea(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: Colors.green,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Looks", style: TextStyle(
+              fontSize: 18,
 
-          ),),
-          Container(
-            height: MediaQuery.of(context).size.height / 3,
-            decoration: BoxDecoration(
-              color: Colors.red,
-            ),
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Container(
-                    width: MediaQuery.of(context).size.width / 3,
-                    decoration: BoxDecoration(color: Colors.blue),
-                    margin: EdgeInsets.only(right: 16),
-                  );
-                }),
-          )
-        ],
+            ),),
+            Container(
+              height: MediaQuery.of(context).size.height / 3,
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width / 3,
+                      decoration: BoxDecoration(color: Colors.blue),
+                      margin: EdgeInsets.only(right: 16),
+                    );
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
