15390e386446ddaf629b280bc2f7e5f1d9b5bc03
Dreamwalker <aristojeff@gmail.com>
2020-04-24 20:47:57 +0900

    Update second_page.dart

--- lib/ep429_fasion_store_app/second_page.dart
+++ lib/ep429_fasion_store_app/second_page.dart
@@ -6,27 +6,22 @@ class SecondPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
+        padding: EdgeInsets.only(left: 16),
         height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-          color: Colors.green,
-        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text("Looks", style: TextStyle(
               fontSize: 18,
-
+              fontWeight: FontWeight.bold,
             ),),
             Container(
               height: MediaQuery.of(context).size.height / 3,
-              decoration: BoxDecoration(
-                color: Colors.red,
-              ),
               child: ListView.builder(
                   scrollDirection: Axis.horizontal,
                   itemBuilder: (context, index) {
                     return Container(
-                      width: MediaQuery.of(context).size.width / 3,
+                      width: MediaQuery.of(context).size.width / 2.8,
                       decoration: BoxDecoration(color: Colors.blue),
                       margin: EdgeInsets.only(right: 16),
                     );
