3ed350445106e12f3bb4e439371cb6e13a76c165
Dreamwalker <aristojeff@gmail.com>
2020-04-25 19:20:15 +0900

    Create detail_page.dart

---
+++ lib/ep429_fasion_store_app/detail_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ShopDetailPage extends StatefulWidget {
+  @override
+  _ShopDetailPageState createState() => _ShopDetailPageState();
+}
+
+class _ShopDetailPageState extends State<ShopDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
