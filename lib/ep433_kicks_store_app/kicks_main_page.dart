560cf6a948802f2f554870ef995940f696414d10
Dreamwalker <aristojeff@gmail.com>
2020-04-26 18:51:19 +0900

    Update kicks_main_page.dart

--- lib/ep433_kicks_store_app/kicks_main_page.dart
+++ lib/ep433_kicks_store_app/kicks_main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:local_auth/local_auth.dart';
 class KicksMainPage extends StatefulWidget {
   @override
@@ -6,9 +7,17 @@ class KicksMainPage extends StatefulWidget {
 }
 
 class _KicksMainPageState extends State<KicksMainPage> {
+  final LocalAuthentication auth = LocalAuthentication();
+  bool _canCheckBiometrics;
+  List<BiometricType> _availableBiometrics;
+  bool _isAuth = false;
   Future<bool> checkAvailable()async{
-    bool canCheckBiometrics = await localAuth.canCheckBiometrics;
+    bool canCheckBiometrics = await auth.canCheckBiometrics;
+    try{
 
+    }on PlatformException catch(e) {
+      
+    }
   }
   @override
   void initState() {
