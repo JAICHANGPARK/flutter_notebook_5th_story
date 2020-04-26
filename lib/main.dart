c525b2e5bee072f9dd011eede4e1148d082b58eb
Dreamwalker <aristojeff@gmail.com>
2020-04-26 18:46:59 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutternotebook5th/ep429_fasion_store_app/main_page.dart';
+import 'package:flutternotebook5th/ep433_kicks_store_app/main_page.dart';
 
 void main(){
-  runApp(FashionStoreApp());
+  runApp(KicksStoreApp());
 }
