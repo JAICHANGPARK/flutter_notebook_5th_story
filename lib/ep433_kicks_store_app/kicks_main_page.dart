778aa4efb31e739581e94867f02d1d56b229a6d5
Dreamwalker <aristojeff@gmail.com>
2020-04-26 18:46:38 +0900

    Create kicks_main_page.dart

---
+++ lib/ep433_kicks_store_app/kicks_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class KicksMainPage extends StatefulWidget {
+  @override
+  _KicksMainPageState createState() => _KicksMainPageState();
+}
+
+class _KicksMainPageState extends State<KicksMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
