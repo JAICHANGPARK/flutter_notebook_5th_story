d93d53a7057c53d016207f8d8316dd98a0feba09
Dreamwalker <aristojeff@gmail.com>
2020-04-26 18:46:03 +0900

    Create main_page.dart

---
+++ lib/ep433_kicks_store_app/main_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class KicksStoreApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: KicksMainPage(),
+    );
+  }
+}
