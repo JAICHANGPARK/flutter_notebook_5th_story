6c2883d2d8edc30ca89231d4aa3506ad1d349bb0
Dreamwalker <aristojeff@gmail.com>
2020-04-26 18:49:37 +0900

    Adds

--- lib/ep433_kicks_store_app/kicks_main_page.dart
+++ lib/ep433_kicks_store_app/kicks_main_page.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
-
+import 'package:local_auth/local_auth.dart';
 class KicksMainPage extends StatefulWidget {
   @override
   _KicksMainPageState createState() => _KicksMainPageState();
 }
 
 class _KicksMainPageState extends State<KicksMainPage> {
+  Future<bool> checkAvailable()async{
+    bool canCheckBiometrics = await localAuth.canCheckBiometrics;
+
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
