bb2daa8966f9084caeddb524ebece0da6b61948f
Dreamwalker <aristojeff@gmail.com>
2020-04-28 20:03:47 +0900

    Create kicks_home_page.dart

---
+++ lib/ep433_kicks_store_app/kicks_home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class KickShopHomePage extends StatefulWidget {
+  @override
+  _KickShopHomePageState createState() => _KickShopHomePageState();
+}
+
+class _KickShopHomePageState extends State<KickShopHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
