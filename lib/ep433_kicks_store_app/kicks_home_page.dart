2ccfaf750938f8255ff235d16f79477e14586ae2
Dreamwalker <aristojeff@gmail.com>
2020-04-28 20:07:07 +0900

    Adds

--- lib/ep433_kicks_store_app/kicks_home_page.dart
+++ lib/ep433_kicks_store_app/kicks_home_page.dart
@@ -9,6 +9,32 @@ class KickShopHomePage extends StatefulWidget {
 class _KickShopHomePageState extends State<KickShopHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 64,
+          child: Row(
+            children: [
+              IconButton(
+                icon: Icon(Icons.home), onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.search), onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.favorite_border), onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.shop), onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.home), onPressed: () {},
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
