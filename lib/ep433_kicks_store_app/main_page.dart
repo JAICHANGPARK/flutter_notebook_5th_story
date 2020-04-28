25ed5059b6f7613a1afca30e14fafe62a3074bd7
Dreamwalker <aristojeff@gmail.com>
2020-04-28 20:04:01 +0900

    Update main_page.dart

--- lib/ep433_kicks_store_app/main_page.dart
+++ lib/ep433_kicks_store_app/main_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutternotebook5th/ep433_kicks_store_app/kicks_main_page.dart';
-
+import 'package:flutternotebook5th/ep433_kicks_store_app/kicks_home_page.dart';
 
 class KicksStoreApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: KicksMainPage(),
+      home: KickShopHomePage(),
     );
   }
 }
