a94a1abd9aae1de558f6b39cd9fa88fb487a2ac2
Dreamwalker <aristojeff@gmail.com>
2020-04-30 18:31:26 +0900

    Update main_page.dart

--- lib/ep437_survey_app/main_page.dart
+++ lib/ep437_survey_app/main_page.dart
@@ -13,7 +13,7 @@ class SurveyApp extends StatelessWidget {
 class SurveyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
 
