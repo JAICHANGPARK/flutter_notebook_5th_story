97cc7a9434ff912f56540783c0c043bc0138a534
Dreamwalker <aristojeff@gmail.com>
2020-04-30 18:31:10 +0900

    Update main_page.dart

--- lib/ep437_survey_app/main_page.dart
+++ lib/ep437_survey_app/main_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 class SurveyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: SurveyHomePage(),
+    );
   }
 }
