97ba4ef5f6f1d571a3edf3c89f1cf18221fd014e
Dreamwalker <aristojeff@gmail.com>
2020-04-30 18:35:01 +0900

    Update main_page.dart

--- lib/ep437_survey_app/main_page.dart
+++ lib/ep437_survey_app/main_page.dart
@@ -13,6 +13,19 @@ class SurveyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            child: Container(
+              height: MediaQuery.of(context).size.height / 3,
+              color: Colors.tealAccent,
+            ),
+          )
+        ],
+      ),
       bottomNavigationBar: _buildBottomNavigationBar(),
     );
   }
