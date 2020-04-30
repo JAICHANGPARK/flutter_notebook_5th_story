435cc70e6a4c94735c892a76227cae0594e04a60
Dreamwalker <aristojeff@gmail.com>
2020-04-30 18:34:23 +0900

    Update main_page.dart

--- lib/ep437_survey_app/main_page.dart
+++ lib/ep437_survey_app/main_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class SurveyApp extends StatelessWidget {
@@ -14,58 +13,17 @@ class SurveyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-
-          items: [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.calendar_today),
-          title: Text("Daily")
-        ),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.payment),
-            title: Text("Cash Back")
-        ),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.check),
-            title: Text("Earn")
-        ),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.payment),
-            title: Text("Cash Out")
-        ),BottomNavigationBarItem(
-            icon: Icon(Icons.apps),
-            title: Text("more")
-        )
-
-      ]),
+      bottomNavigationBar: _buildBottomNavigationBar(),
     );
   }
 
+  Widget _buildBottomNavigationBar() {
+    return BottomNavigationBar(type: BottomNavigationBarType.fixed, items: [
+      BottomNavigationBarItem(icon: Icon(Icons.calendar_today), title: Text("Daily")),
+      BottomNavigationBarItem(icon: Icon(Icons.payment), title: Text("Cash Back")),
+      BottomNavigationBarItem(icon: Icon(Icons.check), title: Text("Earn")),
+      BottomNavigationBarItem(icon: Icon(Icons.payment), title: Text("Cash Out")),
+      BottomNavigationBarItem(icon: Icon(Icons.apps), title: Text("more"))
+    ]);
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
