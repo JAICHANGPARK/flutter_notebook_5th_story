f55ed90b2a1df465388cd35f3d32ee24382e5ffa
Dreamwalker <aristojeff@gmail.com>
2020-05-04 20:43:57 +0900

    Update e_l_home_page.dart

--- lib/ep441_e_learning_app/e_l_home_page.dart
+++ lib/ep441_e_learning_app/e_l_home_page.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 class ELearningHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+
+        ],
+      ),
+    );
   }
 }
