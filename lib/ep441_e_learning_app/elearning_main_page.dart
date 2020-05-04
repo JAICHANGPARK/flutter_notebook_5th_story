05c2cf7ab3a6f2664b7803299c0aeff9ccfd63ae
Dreamwalker <aristojeff@gmail.com>
2020-05-04 20:40:40 +0900

    Update elearning_main_page.dart

--- lib/ep441_e_learning_app/elearning_main_page.dart
+++ lib/ep441_e_learning_app/elearning_main_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutternotebook5th/ep441_e_learning_app/e_l_home_page.dart';
 class ELearningApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
