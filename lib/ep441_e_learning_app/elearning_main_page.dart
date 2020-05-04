baf98fcda750c1bb35a8f2cbc67595072f2d2850
Dreamwalker <aristojeff@gmail.com>
2020-05-04 20:39:24 +0900

    Update elearning_main_page.dart

--- lib/ep441_e_learning_app/elearning_main_page.dart
+++ lib/ep441_e_learning_app/elearning_main_page.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class ELearningApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ELearningHomePage(),
+    );
+  }
+}
