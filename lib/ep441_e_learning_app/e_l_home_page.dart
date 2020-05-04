536ed138c8fafa7e5c2ce86b9f9afc05b910b769
Dreamwalker <aristojeff@gmail.com>
2020-05-04 20:48:13 +0900

    Update e_l_home_page.dart

--- lib/ep441_e_learning_app/e_l_home_page.dart
+++ lib/ep441_e_learning_app/e_l_home_page.dart
@@ -4,31 +4,34 @@ class ELearningHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            left: 0,
-            right: 0,
-            top: 0,
-            child: Column(
-              children: [
-                Container(
-                  height: 64,
-                  width: 64,
-                  decoration: BoxDecoration(
-                    border: Border.all(),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 64,
+                    width: 64,
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                    ),
                   ),
-                ),
-                Text("Select a"),
-                Text("Plan To Learn"),
+                  Text("Select a"),
+                  Text("Plan To Learn"),
 
-                SizedBox(
-                    width: MediaQuery.of(context).size.width / 1.5,
-                    child: Text('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'))
-              ],
-            ),
-          )
-        ],
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width / 1.5,
+                      child: Text('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'))
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
