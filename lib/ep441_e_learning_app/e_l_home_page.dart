4f36df6c8617bad89fc2035b7bf6d962c562bd94
Dreamwalker <aristojeff@gmail.com>
2020-05-04 20:45:42 +0900

    Update e_l_home_page.dart

--- lib/ep441_e_learning_app/e_l_home_page.dart
+++ lib/ep441_e_learning_app/e_l_home_page.dart
@@ -9,7 +9,12 @@ class ELearningHomePage extends StatelessWidget {
           Positioned(
             left: 0,
             right: 0,
-            
+            top: 0,
+            child: Column(
+              children: [
+                
+              ],
+            ),
           )
         ],
       ),
