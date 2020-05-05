ca9263d35ec3afd5f0f83623d208b06464191c7a
Dreamwalker <aristojeff@gmail.com>
2020-05-05 19:21:36 +0900

    Update home_card_item.dart

--- lib/ep441_e_learning_app/home_card_item.dart
+++ lib/ep441_e_learning_app/home_card_item.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomeCardItem extends StatelessWidget {
+  final Color cardColor;
+  final BorderRadiusGeometry borderRadiusGeometry;
+  final String title;
+  final String img;
+
+  HomeCardItem({this.cardColor, this.borderRadiusGeometry, this.title, this.img});
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -12,14 +19,7 @@ class HomeCardItem extends StatelessWidget {
           bottom: 0,
           top: 8,
           child: Container(
-            decoration: BoxDecoration(
-                color: Colors.orange[200],
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(24),
-                  topRight: Radius.circular(24),
-                  bottomRight: Radius.circular(160),
-                  bottomLeft: Radius.circular(24),
-                )),
+            decoration: BoxDecoration(color: cardColor, borderRadius: borderRadiusGeometry),
           ),
         ),
         Positioned(
@@ -35,15 +35,14 @@ class HomeCardItem extends StatelessWidget {
                 child: Padding(
                   padding: const EdgeInsets.only(top: 8),
                   child: Text(
-                    "IELTS".toUpperCase(),
+                    title.toUpperCase(),
                     style: GoogleFonts.montserrat(color: Colors.white, fontSize: 12),
                   ),
                 ),
               ),
               Expanded(
                 flex: 4,
-                child: Image.network(
-                    "https://assets-ouch.icons8.com/thumb/853/0f654408-ed3c-4fea-812f-35185d116b98.png"),
+                child: Image.network(img),
               ),
             ],
           ),
