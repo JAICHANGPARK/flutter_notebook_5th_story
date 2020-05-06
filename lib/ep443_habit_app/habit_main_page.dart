843f04a174cfa9c615cb80f4343784e866e759c0
Dreamwalker <aristojeff@gmail.com>
2020-05-06 19:58:13 +0900

    Update habit_main_page.dart

---
+++ lib/ep443_habit_app/habit_main_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HabitMainPage extends StatefulWidget {
+  @override
+  _HabitMainPageState createState() => _HabitMainPageState();
+}
+
+class _HabitMainPageState extends State<HabitMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+
+        ],
+      ),
+    );
+  }
+}
