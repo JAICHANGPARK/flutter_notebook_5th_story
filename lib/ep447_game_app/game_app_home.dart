55ee06e7d0a650e149be1153e09ee8ae25cd5f38
Dreamwalker <aristojeff@gmail.com>
2020-05-10 19:50:40 +0900

    Update game_app_home.dart

--- lib/ep447_game_app/game_app_home.dart
+++ lib/ep447_game_app/game_app_home.dart
@@ -5,7 +5,7 @@ class GameAppHome extends StatefulWidget {
   _GameAppHOmeState createState() => _GameAppHOmeState();
 }
 
-class _GameAppHOmeState extends State<GameAppHome> with TickerProviderStateMixin{
+class _GameAppHOmeState extends State<GameAppHome> with TickerProviderStateMixin {
   AnimationController _animationController;
   Animation<double> _animation;
   int _tabIndex = 0;
@@ -17,13 +17,10 @@ class _GameAppHOmeState extends State<GameAppHome> with TickerProviderStateMixin
 
     _animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 100));
     _animation = Tween(begin: -0.2, end: 0.2).animate(_animationController);
-    _animationController..addListener(() {
-      setState(() {
+    _animationController
+      ..addListener(() {
+        setState(() {});
       });
-    });
-
-    
-
   }
 
   @override
@@ -79,10 +76,13 @@ class _GameAppHOmeState extends State<GameAppHome> with TickerProviderStateMixin
                                     crossAxisAlignment: CrossAxisAlignment.center,
                                     mainAxisAlignment: MainAxisAlignment.center,
                                     children: [
-                                      Icon(
-                                        Icons.bookmark_border,
-                                        color: Colors.white,
-                                        size: 32,
+                                      Transform.rotate(
+                                        angle: _animation.value,
+                                        child: Icon(
+                                          Icons.bookmark_border,
+                                          color: Colors.white,
+                                          size: 32,
+                                        ),
                                       ),
                                       Container(
                                         height: 4,
@@ -95,12 +95,42 @@ class _GameAppHOmeState extends State<GameAppHome> with TickerProviderStateMixin
                               : IconButton(
                                   icon: Icon(Icons.bookmark_border),
                                   onPressed: () {
+                                    _animationController.forward();
                                     setState(() {
                                       _tabIndex = 0;
                                     });
                                   },
                                 ),
-                          IconButton(
+                          _tabIndex == 1 ?
+                          Container(
+                            height: 72,
+                            width: 48,
+                            decoration: BoxDecoration(
+                                color: Colors.red,
+                                borderRadius: BorderRadius.circular(24),
+                                gradient: LinearGradient(colors: [
+                                  Colors.red[200],
+                                  Colors.red[400],
+                                  Colors.red,
+                                ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.gamepad,
+                                  color: Colors.white,
+                                  size: 32,
+                                ),
+                                Container(
+                                  height: 4,
+                                  width: 4,
+                                  decoration: BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+                                )
+                              ],
+                            ),
+                          )
+                              :IconButton(
                             icon: Icon(Icons.gamepad),
                             onPressed: () {
                               setState(() {
