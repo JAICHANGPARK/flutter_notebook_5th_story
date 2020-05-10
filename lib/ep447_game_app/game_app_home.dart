5db92ffe67cb47d8ebc1f163cb3ee15b54ec2532
Dreamwalker <aristojeff@gmail.com>
2020-05-10 19:33:10 +0900

    Create game_app_home.dart

---
+++ lib/ep447_game_app/game_app_home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GameAppHome extends StatefulWidget {
+  @override
+  _GameAppHOmeState createState() => _GameAppHOmeState();
+}
+
+class _GameAppHOmeState extends State<GameAppHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
