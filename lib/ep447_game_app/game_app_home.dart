0d056d06fd15580a3da6031e74f4e6f1862d48de
Dreamwalker <aristojeff@gmail.com>
2020-05-13 19:55:32 +0900

    Update game_app_home.dart

--- lib/ep447_game_app/game_app_home.dart
+++ lib/ep447_game_app/game_app_home.dart
@@ -397,19 +397,19 @@ class _GameAppHOmeState extends State<GameAppHome> with TickerProviderStateMixin
                         right: 0,
                         child: Container(
                           width: 84,
-                          child: ListView.builder(
-                            controller: _scrollController,
-                              itemCount: 5,
-                              scrollDirection: Axis.horizontal,
-                              itemBuilder: (context, index) {
-                                return Container(
-                                  width: 240,
-                                  margin: EdgeInsets.all(16),
-                                  decoration: BoxDecoration(
-                                    color: Colors.pink,
-                                  ),
-                                );
-                              }),
+//                          child: ListView.builder(
+//                            controller: _scrollController,
+//                              itemCount: 5,
+//                              scrollDirection: Axis.horizontal,
+//                              itemBuilder: (context, index) {
+//                                return Container(
+//                                  width: 240,
+//                                  margin: EdgeInsets.all(16),
+//                                  decoration: BoxDecoration(
+//                                    color: Colors.pink,
+//                                  ),
+//                                );
+//                              }),
                         ),
                       )
                     ],
